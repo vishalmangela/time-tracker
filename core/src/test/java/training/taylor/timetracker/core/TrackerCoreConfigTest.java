4a27e7cc5ead8a27898463c72d40e93122410d80
Vishal Mangela <73231553+vishalmangela@users.noreply.github.com>
2022-06-22 20:19:06 +0530

    changes done

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -1,8 +1,8 @@
 package training.taylor.timetracker.core;
 
-import static org.junit.Assert.*;
+#import static org.junit.Assert.*;
 
-import org.junit.Test;
+#import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
